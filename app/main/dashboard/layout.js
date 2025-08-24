90a832d55dba732c023c4df47cbb821d3599a272
Miss-Marvis <wondikomahiachimarvis@gmail.com>
2025-08-24 10:14:39 +0100

    Added Suspense Library

--- app/main/dashboard/layout.js
+++ app/main/dashboard/layout.js
@@ -1,11 +1,16 @@
+import { Suspense } from 'react'
 import DashboardPage from './page'
+import { BarLoader } from 'react-spinners'
 
 export default function DashboardLayout() {
 	return (
 		<div className='px-5'>
 			<h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
-
-			<DashboardPage />
+			<Suspense
+				fallback={<BarLoader className='mt-4' width={'100%'} color='#933ea' />}
+			>
+				<DashboardPage />
+			</Suspense>
 		</div>
 	)
 }
