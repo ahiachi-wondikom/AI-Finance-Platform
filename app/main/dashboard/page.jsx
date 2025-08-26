e9bdeade037c5ad26cd1b0a81b9043fbc3ccca4f
Miss-Marvis <wondikomahiachimarvis@gmail.com>
2025-08-26 10:21:17 +0100

    Added Drawer component

--- app/main/dashboard/page.jsx
+++ app/main/dashboard/page.jsx
@@ -1,8 +1,10 @@
+import CreateAccountDrawer from '@/components/create-account-drawer'
+
 export default function DashboardPage() {
 	return (
 		<div className='px-5'>
 			<div>
-				<CreateAccountDrawer></CreateAccountDrawer>
+				<CreateAccountDrawer>Open</CreateAccountDrawer>
 			</div>
 		</div>
 	)
--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -1,7 +1,27 @@
 'use client'
 
-const CreateAccountDrawer = () => {
-	return <div>CreateAccountDrawer</div>
+import { useState } from 'react'
+import {
+	Drawer,
+	DrawerContent,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from './ui/Drawer'
+
+const CreateAccountDrawer = ({ children }) => {
+	const [open, setOpen] = useState(false)
+
+	return (
+		<Drawer open={open} onOpenChange={setOpen}>
+			<DrawerTrigger>{children}</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader>
+					<DrawerTitle>Are you absolutely sure?</DrawerTitle>
+				</DrawerHeader>
+			</DrawerContent>
+		</Drawer>
+	)
 }
 
 export default CreateAccountDrawer
