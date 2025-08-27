dfc4c24f0320b566358a5d55fb7e05b3de21c6ed
Miss-Marvis <wondikomahiachimarvis@gmail.com>
2025-08-27 19:56:22 +0100

    Added create-account-drawer component

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -8,9 +8,29 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from './ui/Drawer'
+import { zodResolver } from '@hookform/resolvers'
+import { accountSchema } from '@/app/lib/schema'
+import { Input } from './ui/Input'
 
 const CreateAccountDrawer = ({ children }) => {
 	const [open, setOpen] = useState(false)
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+		watch,
+		reset,
+	} = useForm({
+		resolver: zodResolver(accountSchema),
+		defaultValues: {
+			name: '',
+			type: 'CURRENT',
+			balance: '',
+			isDefault: false,
+		},
+	})
 
 	return (
 		<Drawer open={open} onOpenChange={setOpen}>
@@ -19,8 +39,36 @@ const CreateAccountDrawer = ({ children }) => {
 				<DrawerHeader>
 					<DrawerTitle>Create New Account</DrawerTitle>
 				</DrawerHeader>
-				<div>
-					<form></form>
+				<div className='px-4 pb-4'>
+					<form className='space-y-4'>
+						<div className='space-y-2'>
+							<label htmlFor='name' className='text-sm font-medium'>
+								Account Name
+							</label>
+							<Input
+								id='name'
+								placeholder='e.g., Main Checking'
+								{...register('name')}
+							/>
+							{errors.name && (
+								<p className='text-sm text-red-500'>{errors.name.message}</p>
+							)}
+						</div>
+
+						<div className='space-y-2'>
+							<label htmlFor='type' className='text-sm font-medium'>
+								Account Type
+							</label>
+							<Input
+								id='type'
+								placeholder='e.g., Main Checking'
+								{...register('name')}
+							/>
+							{errors.name && (
+								<p className='text-sm text-red-500'>{errors.name.message}</p>
+							)}
+						</div>
+					</form>
 				</div>
 			</DrawerContent>
 		</Drawer>
