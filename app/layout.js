d4ad95ef7cf890eb944409bd06b2ebf2d2542973
Miss-Marvis <wondikomahiachimarvis@gmail.com>
2025-08-30 11:19:36 +0100

    Added Sonner component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import HeaderWrapper from '@/components/ui/HeaderWrapper'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({ children }) {
 				<body className={inter.className}>
 					<HeaderWrapper />
 					<main className='min-h-screen'>{children}</main>
-					<Toaster />
+					<Toaster richColors />
 					<footer className='bg-blue-50 py-12'>
 						<div className='container mx-auto px-4 text-center text-gray-600'>
 							<p>Made with 💖 by Marvis</p>
