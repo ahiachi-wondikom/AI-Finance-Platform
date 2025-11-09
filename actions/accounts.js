9fc4e917b10f7060a4b4301c090c631e9e622a1d
Miss-Marvis <wondikomahiachimarvis@gmail.com>
2025-11-09 22:12:21 +0100

    Added transactions page

--- actions/accounts.js
+++ actions/accounts.js
@@ -12,7 +12,7 @@ const serializeTransaction = (obj) => {
 	}
 
 	if (obj.amount) {
-		serialized.amount = obj.amount.toNumber() // ✅ fixed (was using obj.balance)
+		serialized.amount = obj.amount.toNumber()
 	}
 
 	return serialized
@@ -48,5 +48,38 @@ export async function updateDefaultAccount(accountId) {
 		return { success: true, data: serializeTransaction(account) }
 	} catch (error) {
 		return { success: false, error: error.message }
+	}
+}
+
+export async function getAccountWithTransactions(accountId) {
+	const { userId } = await auth()
+
+	if (!userId) throw new error('anauthorized')
+
+	const user = await db.user.findUnique({
+		where: { clerkUserId: userId },
+	})
+
+	if (!user) {
+		throw new Error('User not found')
+	}
+
+	const account = await db.account.findUnique({
+		where: { id: accountId, userId: user.id },
+		include: {
+			transactions: {
+				orderBy: { date: 'desc' },
+			},
+			_count: {
+				select: { transactions: true },
+			},
+		},
+	})
+
+	if (!account) return null
+
+	return {
+		...serializeTransaction(account),
+		transactions: account.transactions.map(serializeTransaction),
 	}
 }
